ffbbc3a1d71bf01be95693397d225265070a46c2
Katie Robinson <katierobinson110589@gmail.com>
2019-01-18 12:47:12 +0000

    initial commit

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,9 @@ const env = process.env.NODE_ENV || 'development';
 
 const knex = require('knex');
 
-const dbConfig = require('../knexfile')[env];
+const dbConfig = env === 'production'
+  ? { client: 'pg', connection: process.env.DATABASE_URL }
+  : require('../knexfile')[env];
 
 const connection = knex(dbConfig);
 
