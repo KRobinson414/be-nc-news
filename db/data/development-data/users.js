9d473f5dc90d7b866b8c03bf397f5336aa7b3a33
Katie Robinson <katierobinson110589@gmail.com>
2019-02-06 09:36:55 +0000

    changed user avatar urls

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -2,27 +2,27 @@ module.exports = [
   {
     username: 'tickle122',
     name: 'Tom Tickle',
-    avatar_url: 'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg',
+    avatar_url: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjjzuf15abgAhWNkhQKHWI0DAYQjRx6BAgBEAU&url=https%3A%2F%2Fwww.kisspng.com%2Fpng-mr-tickle-mr-men-mr-happy-children-s-literature-ti-2888806%2F&psig=AOvVaw33nkyTn8919w-feX7mmOhw&ust=1549531706114637',
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
-    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg',
+    avatar_url: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjEqtGe5qbgAhU65eAKHUoPDSsQjRx6BAgBEAU&url=https%3A%2F%2Fshop.mrmen.com%2Fproducts%2Ffamily-portrait-4-family-members%2F&psig=AOvVaw2MD8S0uhrTJ6OH9SdksZcD&ust=1549531862257070',
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
-    avatar_url: 'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729',
+    avatar_url: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjGrfC35qbgAhWMHRQKHexHCPwQjRx6BAgBEAU&url=http%3A%2F%2Fwww.stickpng.com%2Fimg%2Fat-the-movies%2Fcartoons%2Fmr-men%2Fmr-happy&psig=AOvVaw2eSKVsAI1448cbvRknlB_y&ust=1549531936431131',
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
-    avatar_url: 'https://i.imgur.com/WfX0Neu.jpg',
+    avatar_url: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiEmZDQ5qbgAhX1DmMBHZaoCngQjRx6BAgBEAU&url=http%3A%2F%2Fwww.stickpng.com%2Fimg%2Fat-the-movies%2Fcartoons%2Fmr-men%2Fmr-messy&psig=AOvVaw3jHP7SHBg2uvyge7cdZr-w&ust=1549531966011415',
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
-    avatar_url: 'https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg',
+    avatar_url: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjy4d3l5qbgAhW4AWMBHRv5BAQQjRx6BAgBEAU&url=https%3A%2F%2Fcrotoonia.fandom.com%2Fwiki%2FMr._Bump&psig=AOvVaw1Ymhuk6Yqb4DSooXt8jwkn&ust=1549532022826123',
   },
   {
     username: 'jessjelly',
