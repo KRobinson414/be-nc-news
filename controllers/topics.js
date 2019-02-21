5bccb19a62cbdda632616eac8b1ac9f535ad0aae
Katie Robinson <katierobinson110589@gmail.com>
2019-02-21 14:33:25 +0000

    added avatar_url to articles by topic controller

--- controllers/topics.js
+++ controllers/topics.js
@@ -34,8 +34,10 @@ exports.sendArticlesByTopic = (req, res, next) => {
       'articles.votes',
       'articles.created_at',
       'articles.topic',
+      'users.avatar_url',
     )
     .leftJoin('comments', 'comments.article_id', '=', 'articles.article_id')
+    .fullOuterJoin('users', 'users.username', '=', 'article.created_by')
     .count({ comment_count: 'comments.comment_id' })
     .groupBy('articles.article_id')
     .where({ topic })
